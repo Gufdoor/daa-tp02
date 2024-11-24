dc17ae01509ae100c9018702f975837363fceece
Gufdoor <luna.gabriel@proton.me>
2024-11-24 14:51:27 -0300

    feat: Academy class created

--- daa-tp02/src/main/java/org/daa_tp02/Main.java
+++ daa-tp02/src/main/java/org/daa_tp02/Main.java
@@ -7,11 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static class Academy {
+        int M; // Equipment count
+        int N; // Student count
+        int n; // Exercise count
+        List<Exercise> exercises;
+
+        public Academy(int M, int N, int n) {
+            this.M = M;
+            this.N = N;
+            this.n = n;
+        }
+    }
+
     private static class Exercise {
         int studentId;
         int equipmentId;
-        // In minutes
-        double duration;
+        double duration; // In minutes
 
         public Exercise(int studentId, int equipmentId, double duration) {
             this.studentId = studentId;
@@ -29,23 +41,20 @@ public class Main {
     }
 
     /**
-     * Reads the input file and parses it into a list of exercises.
+     * Reads the input file and parses it into an Academy object.
      *
      * @param filePath the path to the input file
      * @return a list of exercises parsed from the file
      * @throws IOException if the file cannot be read
      */
-    private static List<Exercise> readExercisesFromFile(String filePath) throws IOException {
+    private static Academy readAcademyDataFromFile(String filePath) throws IOException {
         final BufferedReader reader = new BufferedReader(new FileReader(filePath));
         final List<Exercise> exercises = new ArrayList<>();
 
         final int equipmentCount = Integer.parseInt(reader.readLine().split("=")[1].trim());
         final int studentCount = Integer.parseInt(reader.readLine().split("=")[1].trim());
         final int exerciseCount = Integer.parseInt(reader.readLine().split("=")[1].trim());
-
-        System.out.println("Equipment count: " + equipmentCount);
-        System.out.println("Student count: " + studentCount);
-        System.out.println("Total exercises: " + exerciseCount);
+        final Academy academy = new Academy(equipmentCount, studentCount, exerciseCount);
 
         boolean isNotEOF = true;
 
@@ -58,8 +67,9 @@ public class Main {
         }
 
         reader.close();
+        academy.exercises = exercises;
 
-        return exercises;
+        return academy;
     }
 
     private static Exercise parseReadLineToExercise(String line) {
@@ -75,9 +85,9 @@ public class Main {
         final String filePath = "exercises.txt";
 
         try {
-            List<Exercise> exercises = readExercisesFromFile(filePath);
+            final Academy academy = readAcademyDataFromFile(filePath);
 
-            for (Exercise exercise : exercises)
+            for (Exercise exercise : academy.exercises)
                 System.out.println(exercise.toString());
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
