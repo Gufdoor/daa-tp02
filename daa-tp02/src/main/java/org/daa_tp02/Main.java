633ec569f7a11b0ddaee5ada4f6fb2ca26dffbd3
Gufdoor <luna.gabriel@proton.me>
2024-11-24 18:58:36 -0300

    feat: brute force permutation implemented

--- daa-tp02/src/main/java/org/daa_tp02/Main.java
+++ daa-tp02/src/main/java/org/daa_tp02/Main.java
@@ -3,8 +3,7 @@ package org.daa_tp02;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     private static class Academy {
@@ -52,10 +51,7 @@ public class Main {
 
         @Override
         public String toString() {
-            return "Exercise: " +
-                    "studentId = " + studentId +
-                    ", equipmentId = " + equipmentId +
-                    ", duration = " + duration;
+            return "Exercise: " + "studentId = " + studentId + ", equipmentId = " + equipmentId + ", duration = " + duration;
         }
     }
 
@@ -79,14 +75,11 @@ public class Main {
             if (line == null) break;
 
             final Exercise exercise = parseReadLineToExercise(line);
-            Student student = academy.students.stream()
-                    .filter(auxStudent -> auxStudent.studentId == exercise.studentId)
-                    .findFirst()
-                    .orElse(null);
+            Student student = academy.students.stream().filter(auxStudent -> auxStudent.studentId == exercise.studentId).findFirst().orElse(null);
 
             if (student == null) {
                 student = new Student(exercise.studentId);
-                academy.students.add(student);
+                academy.addStudent(student);
             }
 
             student.addExercise(exercise);
@@ -112,15 +105,88 @@ public class Main {
         return new Exercise(studentId, equipmentId, duration);
     }
 
+    /**
+     * For a given group of exercises, process all combinations possible, respecting that each student must complete
+     * its exercises sequence in order.
+     *
+     * @param students a list of Students objects containing all exercises sequences
+     * @param stepPermutation current permutation recursion step
+     * @param permutations result permutations
+     */
+    private static void generatePermutations(List<Student> students, List<Exercise> stepPermutation, List<List<Exercise>> permutations) {
+        if (students.isEmpty()) {
+            permutations.add(new ArrayList<>(stepPermutation));
+
+            return;
+        }
+
+        for (Student student : students) {
+            final List<Exercise> exercises = student.exercises;
+            // Remaining students exercises to process
+            final List<Student> remainingStudents = new ArrayList<>(students);
+
+            remainingStudents.remove(student);
+            stepPermutation.addAll(exercises);
+            generatePermutations(remainingStudents, stepPermutation, permutations);
+            // Remove changes to current permutation and continue to next student
+            stepPermutation.subList(stepPermutation.size() - exercises.size(), stepPermutation.size()).clear();
+        }
+    }
+
+    /**
+     * Calculates the duration time for a given permutation.
+     *
+     * @param M an academy equipments count
+     * @param schedule exercises list from a permutation
+     * @return minutes duration in double
+     */
+    private static double simulateSchedule(int M, List<Exercise> schedule) {
+        final double[] equipmentFreeTimes = new double[M];
+        final Map<Integer, Double> studentsElapsedTimes = new HashMap<>();
+
+        for (Exercise exercise : schedule) {
+            final int equipmentIdIndex = exercise.equipmentId - 1;
+            final double equipmentFreeTime = equipmentFreeTimes[equipmentIdIndex];
+            final double studentsElapsedTime = studentsElapsedTimes.getOrDefault(exercise.studentId, 0.0);
+            final double startTime = Math.max(equipmentFreeTime, studentsElapsedTime);
+            final double finishTime = startTime + exercise.duration;
+
+            equipmentFreeTimes[equipmentIdIndex] = finishTime;
+            System.out.println("Equipment " + exercise.equipmentId + " time: " + finishTime + " minutes");
+            studentsElapsedTimes.put(exercise.studentId, finishTime);
+        }
+
+        // Get max elapsed time from students
+        return Collections.max(studentsElapsedTimes.values());
+    }
+
     public static void main(String[] args) {
         final String filePath = "exercises.txt";
 
         try {
             final Academy academy = readAcademyDataFromFile(filePath);
+            final List<List<Exercise>> permutations = new ArrayList<>();
+            generatePermutations(academy.students, new ArrayList<>(), permutations);
+            System.out.println("Generate permutations count: " + permutations.size());
 
-            for (Student student : academy.students)
-                for (Exercise exercise : student.exercises)
-                    System.out.println(exercise.toString());
+            double minTime = Double.MAX_VALUE;
+            List<Exercise> optimalExercisesOrder = new ArrayList<>();
+
+            for (List<Exercise> permutation : permutations) {
+                double elapsed = simulateSchedule(academy.M, permutation);
+
+                if (elapsed < minTime) {
+                    minTime = elapsed;
+                    optimalExercisesOrder = permutation;
+                }
+            }
+
+            System.out.printf("Menor tempo total: %.2f minutos%n", minTime);
+            System.out.println("Melhor sequência:");
+
+            for (Exercise exercise : optimalExercisesOrder) {
+                System.out.println(exercise);
+            }
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
