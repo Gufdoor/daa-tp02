67937cba1e64ef537a296306eac2d2a2c4402212
Gufdoor <luna.gabriel@proton.me>
2024-11-24 13:29:52 -0300

    feat: Exercise class created

--- daa-tp02/src/main/java/org/daa_tp02/Main.java
+++ daa-tp02/src/main/java/org/daa_tp02/Main.java
@@ -1,6 +1,19 @@
 package org.daa_tp02;
 
 public class Main {
+    static class Exercise {
+        int studentId;
+        int equipmentId;
+        // In minutes
+        double duration;
+
+        public Exercise(int studentId, int equipmentId, double duration) {
+            this.studentId = studentId;
+            this.equipmentId = equipmentId;
+            this.duration = duration;
+        }
+    }
+
     public static void main(String[] args) {
     }
 }
